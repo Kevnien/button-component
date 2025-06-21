89d43f8e4d0c1905fbb054d48df6b8897df63ac4
Kev <kevinn858@gmail.com>
2025-06-20 18:46:47 -0700

    build files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/button-component",
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
