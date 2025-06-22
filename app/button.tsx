7bae8e61f72c5439cd07659bfbcf6addf8369573
Kev <kevinn858@gmail.com>
2025-06-22 11:50:08 -0700

    different variations, react-icons, hover, focus

--- app/button.tsx
+++ app/button.tsx
@@ -1,26 +1,65 @@
+import { MdOutlineStarBorder } from "react-icons/md";
+
 interface ButtonProps {
   textContent?: string;
   size?: Size;
   color?: Color;
+  iconSrc?: string;
+  iconPlacement?: IconPlacement;
 }
-type Size = "small" | "medium" | "large" | "x-large" | "star";
+type Size = "2x-large" | "medium" | "large" | "x-large" | "icon-only";
 type Color = "blue" | "red" | "white" | "blue-clear" | "black";
-const blueStyles = "text-white bg-indigo-700";
-const smallStyles = "text-sm p-3";
-const mediumStyles = "text-md p-4";
+type IconPlacement = "left" | "right" | "both";
+const blueStyles =
+  "text-white bg-indigo-700 hover:bg-indigo-800 hover:text-neutral-50";
+const whiteStyles =
+  "text-neutral-900 bg-white border-solid border-neutral-200 border-1 shadow-sm hover:text-neutral-950 hover:bg-neutral-50";
+const blueClearStyles = "text-indigo-700 hover:text-indigo-800 hover:shadow-sm";
+const redStyles =
+  "text-white bg-red-600 hover:text-neutral-50 hover:bg-red-700";
+const blackStyles = "hover:shadow-sm";
+const mediumStyles = "text-base/4";
+const largeStyles = "text-lg/5";
+const xLargeStyles = "text-lg/5 p-5";
+const xxLargeStyles = "text-xl/6 p-6";
 
 export default function Button({
-  textContent = "Button CTA",
+  textContent,
   size = "medium",
   color = "blue",
+  iconSrc,
+  iconPlacement = iconSrc ? "left" : undefined,
 }: ButtonProps) {
   return (
     <button
-      className={`rounded-sm font-medium m-4 ${
-        size === "small" && smallStyles
-      } ${size === "medium" && mediumStyles} ${color === "blue" && blueStyles}`}
+      className={`flex items-center rounded-sm font-medium m-4 box-border w-fit gap-3 p-4 focus:shadow-lg ${
+        size === "large" && largeStyles
+      } ${size === "medium" && mediumStyles} ${
+        size === "x-large" && xLargeStyles
+      } ${size === "2x-large" && xxLargeStyles} ${
+        color === "blue" && blueStyles
+      } ${color === "white" && whiteStyles} ${
+        color === "blue-clear" && blueClearStyles
+      } ${color === "red" && redStyles} ${color === "black" && blackStyles}`}
+      tabIndex={0}
     >
-      {textContent}
+      {iconPlacement === "left" && (
+        <MdOutlineStarBorder
+          className={`flex items-center ${
+            size === "large" || (size === "x-large" && "size-5")
+          }`}
+        />
+      )}
+      {!(size === "icon-only") && <span>{textContent}</span>}
+      {(size === "icon-only" || iconPlacement === "right") && (
+        <MdOutlineStarBorder
+          className={`flex items-center ${
+            size === "large" ||
+            size === "x-large" ||
+            (size === "icon-only" && "size-5")
+          }`}
+        />
+      )}
     </button>
   );
 }
--- app/buttons.tsx
+++ app/buttons.tsx
@@ -1,10 +1,71 @@
 import Button from "./button";
 
+const vercelSrc = "/button-component/globe.svg";
+const textContent = "Button CTA";
+
 export default function Buttons() {
-    return (
-        <div className="blue-buttons flex m-4 g-4 font-[Noto_Sans]">
-            <Button size="small" />
-            <Button />
-        </div>
-    )
+  return (
+    <>
+      <div className="blue-buttons flex flex-wrap m-4 g-4 items-center font-[Noto_Sans]">
+        <Button textContent={textContent} size="medium" />
+        <Button textContent={textContent} size="large" iconSrc={vercelSrc} />
+        <Button
+          textContent={textContent}
+          size="x-large"
+          iconSrc={vercelSrc}
+          iconPlacement="right"
+        />
+        <Button textContent={textContent} size="2x-large" />
+        <Button size="icon-only" />
+      </div>
+      <div className="white-buttons flex flex-wrap m-4 g-4 items-center font-[Noto_Sans]">
+        <Button color="white" textContent={textContent} size="medium" />
+        <Button color="white" textContent={textContent} size="large" iconSrc={vercelSrc} />
+        <Button color="white"
+          textContent={textContent}
+          size="x-large"
+          iconSrc={vercelSrc}
+          iconPlacement="right"
+        />
+        <Button color="white" textContent={textContent} size="2x-large" />
+        <Button color="white" size="icon-only" />
+      </div>
+      <div className="blue-clear-buttons flex flex-wrap m-4 g-4 items-center font-[Noto_Sans]">
+        <Button color="blue-clear" textContent={textContent} size="medium" />
+        <Button color="blue-clear" textContent={textContent} size="large" iconSrc={vercelSrc} />
+        <Button color="blue-clear"
+          textContent={textContent}
+          size="x-large"
+          iconSrc={vercelSrc}
+          iconPlacement="right"
+        />
+        <Button color="blue-clear" textContent={textContent} size="2x-large" />
+        <Button color="blue-clear" size="icon-only" />
+      </div>
+      <div className="black-buttons flex flex-wrap m-4 g-4 items-center font-[Noto_Sans]">
+        <Button color="black" textContent={textContent} size="medium" />
+        <Button color="black" textContent={textContent} size="large" iconSrc={vercelSrc} />
+        <Button color="black"
+          textContent={textContent}
+          size="x-large"
+          iconSrc={vercelSrc}
+          iconPlacement="right"
+        />
+        <Button color="black" textContent={textContent} size="2x-large" />
+        <Button color="black" size="icon-only" />
+      </div>
+      <div className="red-buttons flex flex-wrap m-4 g-4 items-center font-[Noto_Sans]">
+        <Button color="red" textContent={textContent} size="medium" />
+        <Button color="red" textContent={textContent} size="large" iconSrc={vercelSrc} />
+        <Button color="red"
+          textContent={textContent}
+          size="x-large"
+          iconSrc={vercelSrc}
+          iconPlacement="right"
+        />
+        <Button color="red" textContent={textContent} size="2x-large" />
+        <Button color="red" size="icon-only" />
+      </div>
+    </>
+  );
 }
